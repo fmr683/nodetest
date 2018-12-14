94f3c9f53668a96280e22a1789b5ada149923311
Fazlul <frm683@gmail.com>
2018-12-15 01:23:24 +0530

    updates

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ function gooleSearch() {
     var start = new Date();
     let options = { 
         method: "GET", 
-        uri: 'http://dev7.sentrytest.tltid.com:3000/SegD0=ACR&SegO0=CMB&SegDt1=2019-05-30&SegDt0=2019-05-16', 
+        uri: 'http://localhost:3000/SegD0=ACR&SegO0=CMB&SegDt1=2019-05-30&SegDt0=2019-05-16', 
     };
 
     request(options, function (error, response, body) {
